68fa4a49bc3d4586ba5fee10dd4f19dd33a06454
indium-dinesh <dinesh.subramani@indiumsoft.com>
2021-06-24 10:59:53 +0530

    dynamic file and save redaction code

--- src/app/manual/manual.component.ts
+++ src/app/manual/manual.component.ts
@@ -156,6 +156,168 @@ export class ManualComponent implements OnInit {
         /* apply redaction */
         
       });
+      /*
+      .then(instance => {
+        const { annotManager, docViewer, Annotations, CoreControls } = instance;
+        const annotHistoryManager = docViewer.getAnnotationHistoryManager();
+        // console.log("user", annotManager.getDisplayAuthor(annotManager))
+        instance.loadDocument(base64ToBlob(this.input.result), { filename: 'myfile.pdf' });
+        instance.setTheme('dark');
+        /* hide the tool group *  /
+        instance.disableElements(['toolbarGroup-View']);
+        instance.disableElements(['toolbarGroup-Annotate']);
+        instance.disableElements(['toolbarGroup-Shapes']);
+        // instance.disableElements(['toolbarGroup-Edit']);
+        instance.disableElements(['toolbarGroup-Insert']);
+        /* hide the tool group * /
+        /* setting default tool * /
+        instance.setToolbarGroup('toolbarGroup-Edit', true);
+        /* setting default tool * /
+  
+        instance.contextMenuPopup.update([
+          {
+            type: 'actionButton',
+            img: 'path/to/image',
+            onClick: () => {
+              
+            }
+          },
+          { dataElement: "annotationRedactButton" }
+        ]);
+        instance.textPopup.update([
+          { dataElement: "copyTextButton" },
+          { dataElement: "textRedactToolButton" },
+        ])
+        console.log("annotation popup ", instance.annotationPopup.getItems())
+  
+        /* ---------------------------------------------------------------------------------------------- * /
+        const { Tools } = instance;
+        instance.setToolMode(Tools.ToolNames.REDACTION);
+        // instance.enableFeatures([instance.Feature.Redaction]);
+        let thys = this;
+  
+        /* left nav * /
+        instance.setHeaderItems(function (header) {
+          // console.log("header",header)
+          // console.log("header",header.getHeader('toolbarGroup-Edit').getItems())
+          header.getHeader('default').update([
+            {
+              type: 'toggleElementButton',
+              img: 'icon-header-sidebar-line',
+              element: 'leftPanel',
+              dataElement: 'leftPanelButton'
+            },
+            {
+              type: 'toolButton', toolName: 'Pan'
+            },
+            {
+              type: 'actionButton',
+              img: 'icon-alert',
+              onClick: () => {
+                alert('Hello world!');
+              },
+              dataElement: 'alertButton',
+              hidden: ['mobile']
+            },
+            { type: "spacer", hidden: ['mobile'] },
+            {
+              dataElement: "searchButton",
+              element: "searchPanel",
+              img: "icon-header-search",
+              title: "component.searchPanel",
+              type: "toggleElementButton"
+            },
+            {
+              dataElement: "toggleNotesButton",
+              element: "notesPanel",
+              hidden: ["small-mobile"],
+              img: "icon-header-chat-line",
+              title: "component.notesPanel",
+              type: "toggleElementButton"
+            }
+          ])
+          header.getHeader('toolbarGroup-Edit').update([
+            { type: "spacer", hidden: ['mobile'] },
+            { type: "actionButton", dataElement: "redoButton", title: "action.redo", img: "icon-operation-redo", onClick: () => { annotHistoryManager.redo(); } },
+            { type: "actionButton", dataElement: "undoButton", title: "action.undo", img: "icon-operation-undo", onClick: () => { annotHistoryManager.undo(); } },
+            { type: "toolGroupButton", toolGroup: "redactionTools", dataElement: "redactionToolGroupButton", title: "annotation.redact", showColor: "never" },
+            { type: "spacer", hidden: ['mobile'] },
+            { type: "actionButton", img: "icon-header-download", 
+              onClick: () => {
+                const redactionList = annotManager.getAnnotationsList().filter(annot => annot instanceof Annotations.RedactionAnnotation);
+                let results: any = [];
+                redactionList.map(dat => {
+                  let t = {
+                    id:dat['Sw'],
+                    pageNumber: dat['XB'],
+                    pageHeight: docViewer.getPageHeight(dat['XB']),
+                    pageWidth: docViewer.getPageWidth(dat['XB']),
+                    redactions: dat['Nb']
+                  }
+                  results.push(t)
+                })
+                _.merge(_.keyBy(results, 'id'), _.keyBy(thys.redactionTypes, 'id'))
+                thys.redactionResults = results;
+                console.log("results ", results)
+                console.log(JSON.stringify(results))
+              }
+            },
+  
+          ])
+        });
+  
+        annotManager.setAnnotationDisplayAuthorMap((annotation) => {
+          console.log("annotation ", annotation)
+  
+          if (annotation.Id === '1') {
+            return 'John';
+          } else {
+            return 'sdfas';
+          }
+        });
+  
+        /* user defined redaction
+  
+        docViewer.on('documentLoaded', () => {
+          let redactions:any = [];
+          let redactAnnot = new Annotations.RedactionAnnotation({
+              PageNumber: 1,
+              Rect: new CoreControls.Math.Rect(101, 102, 300, 200) // Rect are in the form x1,y1,x2,y2
+          });
+          redactions.push(redactAnnot)
+          const redactAnnotations = redactions
+          annotManager.addAnnotations(redactAnnotations);
+        })
+        * /
+        docViewer.on('documentLoaded', () => {
+  
+          console.log("cc", docViewer.getPageHeight(1))
+          console.log("cc", docViewer.getPageWidth(1))
+        });
+        console.log("annotation popup", instance.annotationPopup.getItems())
+        annotManager.on('annotationSelected',(annotationsList) =>{
+          console.log("annoation added ",annotationsList)
+        })
+        annotManager.on('annotationChanged', (annotations, action) => {
+          if(action=='add' && annotations[0]['Subject']=='Redact'){
+            let type = ""
+            console.log(annotations)
+            if (confirm("The Redaction part contains Personal details")==true)
+              type = "PPD"//console.log("PPD")
+            else
+              type = "CCI"//console.log("cci")
+            this.redactionTypes.push({
+              id:annotations[0]['Sw'],
+              type:type
+            });
+            console.log("redactionTypes",this.redactionTypes)
+  
+            // this.modalService.open(this.content)
+          }
+        });
+        
+      });
+      */
   }
   url: any;
   input: any
